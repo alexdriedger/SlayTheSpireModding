f2860ba305de610da0ebdcaacb5eed7ca0f603c2
alexdriedger <aldriedger@gmail.com>
2018-10-08 14:33:21 -0700

    Website files

---
+++ website/siteConfig.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const siteConfig = {
+  title: 'Slay the Spire Modding', // Title for your website.
+  tagline: 'Modding Slay the Spire',
+  url: 'https://daviscook477.github.io', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'BaseMod',
+  organizationName: 'daviscook477',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'getting-started', label: 'Docs'},
+    {href: 'https://discord.gg/VC6kRNu', label: 'Discord'},
+  ],
+
+  /* path to images for header/footer */
+  headerIcon: 'img/slay_the_spire.png',
+  favicon: 'img/favicon.png',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#2E8555',
+    secondaryColor: '#205C3B',
+  },
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Slay the Spire Modding Community`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'darcula',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+};
+
+module.exports = siteConfig;
