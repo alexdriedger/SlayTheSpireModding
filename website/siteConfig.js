71abecf3ecd12f740494195fc74112631ce6478f
alexdriedger <aldriedger@gmail.com>
2018-10-11 01:38:33 -0700

    Updated baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,7 +9,7 @@ const siteConfig = {
   title: 'Slay the Spire Modding', // Title for your website.
   tagline: 'Modding Slay the Spire',
   url: 'https://alexdriedger.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/SlayTheSpireModding/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
